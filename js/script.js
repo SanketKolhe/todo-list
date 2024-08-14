7e96acb6d1c0227513814487ebf90acaf3d3528b
Sanket Kolhe <sanket11264@gmail.com>
2024-08-14 14:32:29 +0530

    Added edit functionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const inputTodo = document.getElementById('input_todo');
 const addTodo = document.getElementById('add_todo');
 const todoDisplay = document.getElementById('todo-display');
 
+let edit = null;
+
 const add = () => {
     const inputText = inputTodo.value.trim();
 
@@ -10,31 +12,43 @@ const add = () => {
         return false;
     }
 
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-    p.innerHTML = inputText;
-    li.appendChild(p);
+    if (addTodo.value === 'Edit') {
+        edit.target.previousElementSibling.innerHTML = inputText;
+        addTodo.value = 'Add'
+        inputTodo.value = '';
+    } else {
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerHTML = inputText;
+        li.appendChild(p);
 
-    // Edit button
-    const editTodo = document.createElement('button');
-    editTodo.innerText='Edit';
-    editTodo.classList.add('action-btn', 'edit-btn');
-    li.appendChild(editTodo);
+        // Edit button
+        const editTodo = document.createElement('button');
+        editTodo.innerText = 'Edit';
+        editTodo.classList.add('action-btn', 'edit-btn');
+        li.appendChild(editTodo);
 
-    // Delete button
-    const deleteTodo = document.createElement('button');
-    deleteTodo.innerText='Remove';
-    deleteTodo.classList.add('action-btn', 'delete-btn');
-    li.appendChild(deleteTodo);
+        // Delete button
+        const deleteTodo = document.createElement('button');
+        deleteTodo.innerText = 'Remove';
+        deleteTodo.classList.add('action-btn', 'delete-btn');
+        li.appendChild(deleteTodo);
 
-    todoDisplay.appendChild(li);
-    inputTodo.value = "";
+        todoDisplay.appendChild(li);
+        inputTodo.value = "";
+    }
 }
 
-const changeTodo = (e)=> {
-    if(e.target.innerText === 'Remove') 
-    {
+const changeTodo = (e) => {
+    if (e.target.innerHTML === 'Remove') {
         todoDisplay.removeChild(e.target.parentElement);
+    }
+
+    if (e.target.innerHTML === 'Edit') {
+        inputTodo.value = e.target.previousElementSibling.innerHTML;
+        inputTodo.focus();
+        addTodo.value = 'Edit'
+        edit = e;
     }
 }
 
