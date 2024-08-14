58a698e07afff6408e79e37561ecb625c812da16
Sanket Kolhe <sanket11264@gmail.com>
2024-08-14 14:07:49 +0530

    Added delete functionality

--- js/script.js
+++ js/script.js
@@ -14,8 +14,29 @@ const add = () => {
     const p = document.createElement('p');
     p.innerHTML = inputText;
     li.appendChild(p);
+
+    // Edit button
+    const editTodo = document.createElement('button');
+    editTodo.innerText='Edit';
+    editTodo.classList.add('action-btn', 'edit-btn');
+    li.appendChild(editTodo);
+
+    // Delete button
+    const deleteTodo = document.createElement('button');
+    deleteTodo.innerText='Remove';
+    deleteTodo.classList.add('action-btn', 'delete-btn');
+    li.appendChild(deleteTodo);
+
     todoDisplay.appendChild(li);
     inputTodo.value = "";
 }
 
+const changeTodo = (e)=> {
+    if(e.target.innerText === 'Remove') 
+    {
+        todoDisplay.removeChild(e.target.parentElement);
+    }
+}
+
 addTodo.addEventListener('click', add);
+todoDisplay.addEventListener('click', changeTodo);
