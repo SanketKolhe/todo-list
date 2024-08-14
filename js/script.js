fab82fca49b6f0b96ed18b29d603d3c65bafa291
Sanket Kolhe <sanket11264@gmail.com>
2024-08-14 19:49:19 +0530

    added option to save todo list at local storage, added edit and remove functionality for todo list in local storage

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const todoDisplay = document.getElementById('todo-display');
 
 let edit = null;
 
+// To add todo list item
 const add = () => {
     const inputText = inputTodo.value.trim();
 
@@ -13,8 +14,10 @@ const add = () => {
     }
 
     if (addTodo.value === 'Edit') {
+        const inputTodoText = edit.target.previousElementSibling.innerHTML;
         edit.target.previousElementSibling.innerHTML = inputText;
-        addTodo.value = 'Add'
+        editLocal(inputTodoText, inputText);
+        addTodo.value = 'Add';
         inputTodo.value = '';
     } else {
         const li = document.createElement('li');
@@ -36,12 +39,16 @@ const add = () => {
 
         todoDisplay.appendChild(li);
         inputTodo.value = "";
+        saveLocal(inputText);
     }
-}
+};
 
+
+// To edit/remove todo list item
 const changeTodo = (e) => {
     if (e.target.innerHTML === 'Remove') {
         todoDisplay.removeChild(e.target.parentElement);
+        deleteLocal(e.target.parentElement);
     }
 
     if (e.target.innerHTML === 'Edit') {
@@ -50,7 +57,74 @@ const changeTodo = (e) => {
         addTodo.value = 'Edit'
         edit = e;
     }
-}
+};
 
+// To save todo list item in local storage
+const saveLocal = (todo) => {
+    let todoList;
+    if (localStorage.getItem('todoList') === null) {
+        todoList = [];
+    } else {
+        todoList = JSON.parse(localStorage.getItem('todoList'));
+    }
+    todoList.push(todo);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+};
+
+// To get todo list item from local storage
+const getLocal = () => {
+    let todoList;
+    if (localStorage.getItem('todoList') === null) {
+        todoList = [];
+    } else {
+        todoList = JSON.parse(localStorage.getItem('todoList'));
+        todoList.forEach(todo => {
+            const li = document.createElement('li');
+            const p = document.createElement('p');
+            p.innerHTML = todo;
+            li.appendChild(p);
+
+            // Edit button
+            const editTodo = document.createElement('button');
+            editTodo.innerText = 'Edit';
+            editTodo.classList.add('action-btn', 'edit-btn');
+            li.appendChild(editTodo);
+
+            // Delete button
+            const deleteTodo = document.createElement('button');
+            deleteTodo.innerText = 'Remove';
+            deleteTodo.classList.add('action-btn', 'delete-btn');
+            li.appendChild(deleteTodo);
+
+            todoDisplay.appendChild(li);
+        });
+    }
+};
+
+// To delete todo list item from local storage
+const deleteLocal = (todo) => {
+    let todoList;
+    if (localStorage.getItem('todoList') === null) {
+        todoList = [];
+    } else {
+        todoList = JSON.parse(localStorage.getItem('todoList'));
+    }
+    let todoText = todo.children[0].innerHTML;
+    let todoIndex = todoList.indexOf(todoText);
+    todoList.splice(todoIndex, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+};
+
+
+// To delete todo list item from local storage
+const editLocal = (oldText, newText) => {
+    let todoList = JSON.parse(localStorage.getItem('todoList'));
+    let todoIndex = todoList.indexOf(oldText);
+    todoList[todoIndex] = newText;
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+};
+
+
+document.addEventListener('DOMContentLoaded', getLocal);
 addTodo.addEventListener('click', add);
 todoDisplay.addEventListener('click', changeTodo);
